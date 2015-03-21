d51a661fff28b77d76ebe76f57ad4f4abec8e687
mz2 <matias.piipari@gmail.com>
2015-03-21 13:57:23 +0000

    Commandline tool now functional.

--- ASCIImage/main.m
+++ ASCIImage/main.m
@@ -22,25 +22,27 @@ int main(int argc, const char * argv[]) {
         NSData *inputData = [NSData dataWithData:[input readDataToEndOfFile]];
         NSString *inputString = [[NSString alloc] initWithData:inputData encoding:NSUTF8StringEncoding];
 
-        FSArgumentSignature *antialiasArg = [FSArgumentSignature argumentSignatureWithFormat:@"[-a --antialias]"];
+        FSArgumentSignature *antialiasArg = [FSArgumentSignature argumentSignatureWithFormat:@"[-a --no-antialias]"];
         FSArgumentSignature *closeArg = [FSArgumentSignature argumentSignatureWithFormat:@"[-c --close]"];
         FSArgumentSignature *strokeWidthArg = [FSArgumentSignature argumentSignatureWithFormat:@"[-s --stroke]="];
         FSArgumentSignature *scaleArg = [FSArgumentSignature argumentSignatureWithFormat:@"[-z --scale]="];
        
         NSArray *signatures = @[ antialiasArg, closeArg, strokeWidthArg, scaleArg ];
         
-        FSArgumentPackage * package = [NSProcessInfo.processInfo fsargs_parseArgumentsWithSignatures:signatures];
+        FSArgumentPackage *package = [NSProcessInfo.processInfo fsargs_parseArgumentsWithSignatures:signatures];
         
-        BOOL antialias = [package booleanValueForSignature:antialiasArg];
+        BOOL antialias = ![package booleanValueForSignature:antialiasArg];
         BOOL close = [package booleanValueForSignature:closeArg];
         
         id strokeWidthObj = [package firstObjectForSignature:strokeWidthArg] ;
         CGFloat strokeWidth = strokeWidthObj ? [strokeWidthObj floatValue] : 1.0f;
         
-        id scaleObj = [package firstObjectForSignature:closeArg];
-        CGFloat scale = scaleObj ? [scaleObj floatValue] : 1.0f;
+        id scaleObj = [package firstObjectForSignature:scaleArg];
+        CGFloat scale = scaleObj ? [scaleObj floatValue] : 20.0f;
         
         MPLayeredASCIImageRenderer *renderer = [MPLayeredASCIImageRenderer new];
+        
+        NSLog(@"Close: %@", @(close));
         
         NSImage *img = [renderer imageWithLayeredASCIIString:inputString
                                                        scale:scale
@@ -50,13 +52,18 @@ int main(int argc, const char * argv[]) {
                                                   closeShape:close
                                                    antialias:antialias];
         
-        CGImageRef cgRef = [img CGImageForProposedRect:NULL context:nil hints:nil];
-        NSBitmapImageRep *newRep = [[NSBitmapImageRep alloc] initWithCGImage:cgRef];
-        [newRep setSize:[img size]];   // if you want the same resolution
-        NSData *pngData = [newRep representationUsingType:NSPNGFileType properties:nil];
-        
-        NSFileHandle *stdout = [NSFileHandle fileHandleWithStandardOutput];
-        [stdout writeData: pngData];
+        if (img) {
+            CGImageRef cgRef = [img CGImageForProposedRect:NULL context:nil hints:nil];
+            NSBitmapImageRep *newRep = [[NSBitmapImageRep alloc] initWithCGImage:cgRef];
+            [newRep setSize:[img size]];   // if you want the same resolution
+            NSData *pngData = [newRep representationUsingType:NSPNGFileType properties:nil];
+            
+            NSFileHandle *stdout = [NSFileHandle fileHandleWithStandardOutput];
+            [stdout writeData: pngData];
+        }
+        else {
+            return -1;
+        }
     }
     return 0;
 }
