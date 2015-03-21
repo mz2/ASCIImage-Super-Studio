6d9f44adabb6db885295a930676eaa020e189abc
mz2 <matias.piipari@gmail.com>
2015-03-21 00:41:11 +0000

    Adds experimental support for layering by means of using '#>>>\n' as a separator between layers

--- ASCIImagination/MPASCIImagePreviewViewController.m
+++ ASCIImagination/MPASCIImagePreviewViewController.m
@@ -36,66 +36,106 @@
     [self ASCIImageTextViewControllerDidRefresh:self.textViewController];
 }
 
+// from http://stackoverflow.com/questions/9159931/compositing-images-with-fresh-context
+- (void)compositeImage:(NSImage *)overlay
+                      onto:(NSImage *)background
+                   atPoint:(NSPoint)location {
+    
+    [background lockFocus];
+    [[NSGraphicsContext currentContext] setImageInterpolation:NSImageInterpolationHigh];
+    [overlay drawInRect:NSMakeRect(location.x, location.y, [overlay size].width, [overlay size].width)
+               fromRect:NSZeroRect
+              operation:NSCompositeSourceOver
+               fraction:1.0];
+    [background unlockFocus];
+}
+
 - (void)ASCIImageTextViewControllerDidRefresh:(MPASCIImageTextViewController *)textViewController {
-    NSMutableArray *asciiStrs = [[textViewController.editorTextArea string] componentsSeparatedByString:@"\n"].mutableCopy;
+
+    NSMutableArray *layers = [[textViewController.editorTextArea string] componentsSeparatedByString:@"#>>>\n"].mutableCopy;
+    NSMutableArray *images = [NSMutableArray new];
     
-    NSUInteger len = [asciiStrs[0] length];
-    for (NSUInteger i = 0; i < asciiStrs.count; i++) {
-        NSString *str = asciiStrs[i];
+    for (NSString *layerStr in layers) {
+        NSMutableArray *asciiStrs = [layerStr componentsSeparatedByString:@"\n"].mutableCopy;
         
-        if (str.length > len) {
-            str = [str substringToIndex:len];
+        NSUInteger len = [asciiStrs[0] length];
+        for (NSUInteger i = 0; i < asciiStrs.count; i++) {
+            NSString *str = asciiStrs[i];
+            
+            if (str.length > len) {
+                str = [str substringToIndex:len];
+            }
+            else if (str.length < len) {
+                str = [str stringWithLinesPaddedToLength:len];
+            }
+            
+            asciiStrs[i] = str;
         }
-        else if (str.length < len) {
-            str = [str stringWithLinesPaddedToLength:len];
-        }
         
-        asciiStrs[i] = str;
-    }
-    
-    PARImage *img = [PARImage imageWithASCIIRepresentation:asciiStrs contextHandler:^(NSMutableDictionary *context) {
-        
-        CGFloat scaleFactor = [[NSUserDefaults standardUserDefaults] floatForKey:@"previewScale"];
-        if (scaleFactor <= 0)
-            scaleFactor = 20.0f;
-        
-        context[ASCIIContextScale] = @(20.0f);
-        
-        CGFloat strokeWidth = [[NSUserDefaults standardUserDefaults] floatForKey:@"strokeWidth"];
-        if (strokeWidth <= 0)
-            strokeWidth = 1.0f;
-        
-        context[ASCIIContextLineWidth] = @(strokeWidth);
-        
-        NSData *colorData = [[NSUserDefaults standardUserDefaults] objectForKey:@"strokeColor"];
-        
-        NSColor *defaultColor = [NSColor blackColor];
-        NSColor *color;
-        if (colorData) {
-            color = [NSUnarchiver unarchiveObjectWithData:colorData];
-            if (!color) {
+        PARImage *img = [PARImage imageWithASCIIRepresentation:asciiStrs contextHandler:^(NSMutableDictionary *context) {
+            
+            CGFloat scaleFactor = [[NSUserDefaults standardUserDefaults] floatForKey:@"previewScale"];
+            if (scaleFactor <= 0)
+                scaleFactor = 20.0f;
+            
+            context[ASCIIContextScale] = @(20.0f);
+            
+            CGFloat strokeWidth = [[NSUserDefaults standardUserDefaults] floatForKey:@"strokeWidth"];
+            if (strokeWidth <= 0)
+                strokeWidth = 1.0f;
+            
+            context[ASCIIContextLineWidth] = @(strokeWidth);
+            
+            NSData *colorData = [[NSUserDefaults standardUserDefaults] objectForKey:@"strokeColor"];
+            
+            NSColor *defaultColor = [NSColor blackColor];
+            NSColor *color;
+            if (colorData) {
+                color = [NSUnarchiver unarchiveObjectWithData:colorData];
+                if (!color) {
+                    color = defaultColor;
+                }
+            }
+            else {
                 color = defaultColor;
             }
-        }
-        else {
-            color = defaultColor;
-        }
-        context[ASCIIContextStrokeColor] = color;
+            context[ASCIIContextStrokeColor] = color;
+            
+            NSData *fillColorData = [[NSUserDefaults standardUserDefaults] objectForKey:@"fillColor"];
+            
+            if (fillColorData) {
+                NSColor *fillColor = [NSUnarchiver unarchiveObjectWithData:colorData] ?: [NSColor clearColor];
+                context[ASCIIContextFillColor] = fillColor;
+            }
+            
+            context[ASCIIContextShouldClose] = @([[NSUserDefaults standardUserDefaults] boolForKey:@"shouldClose"]);
+            
+            BOOL antialias = [[NSUserDefaults standardUserDefaults] boolForKey:@"antialias"];
+            context[ASCIIContextShouldAntialias] = @(antialias);
+        }];
         
-        NSData *fillColorData = [[NSUserDefaults standardUserDefaults] objectForKey:@"fillColor"];
-        
-        if (fillColorData) {
-            NSColor *fillColor = [NSUnarchiver unarchiveObjectWithData:colorData] ?: [NSColor clearColor];
-            context[ASCIIContextFillColor] = fillColor;            
-        }
-
-        context[ASCIIContextShouldClose] = @([[NSUserDefaults standardUserDefaults] boolForKey:@"shouldClose"]);
-        
-        BOOL antialias = [[NSUserDefaults standardUserDefaults] boolForKey:@"antialias"];
-        context[ASCIIContextShouldAntialias] = @(antialias);
-    }];
+        [images addObject:img];
+    }
     
-    self.previewArea.image = img;
+    if (images.count == 1) {
+        self.previewArea.image = images[0];
+    }
+    else if (images.count > 0) {
+        NSImage *accummulatingImage = [images[0] copy];
+        
+        if (accummulatingImage.size.width > 0 && accummulatingImage.size.height > 0) {
+            for (NSImage *img in [images subarrayWithRange:NSMakeRange(1, images.count - 1)]) {
+                if (img.size.width > 0 && img.size.height > 0) {
+                    [self compositeImage:img onto:accummulatingImage atPoint:NSMakePoint(0, 0)];
+                }
+            }
+        }
+        
+        self.previewArea.image = accummulatingImage;
+    }
+    else {
+        self.previewArea.image = nil;
+    }
 }
 
 @end
